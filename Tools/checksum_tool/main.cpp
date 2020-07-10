04ab7ba1edf5033e0e12b9018bcc283abe7bb044
IvanDSM <snesivan1@gmail.com>
2020-07-10 06:18:20 -0300

    checksum_tool: Add support for scanning multiple files.

--- Tools/checksum_tool/main.cpp
+++ Tools/checksum_tool/main.cpp
@@ -9,35 +9,48 @@ int main(int argc, char *argv[])
 
     if (argc < 2)
     {
-        qInfo() << "No file provided. Exiting.";
+        qInfo() << "No files provided. Exiting.";
         return 1;
     }
 
-    QFile inFile(argv[1]);
-    if (!inFile.open(QIODevice::ReadOnly))
+    for (int argi = 1; argi < argc; argi++)
     {
-        qInfo() << "Couldn't open the file. Exiting.";
-        return 1;
+        QFile inFile(argv[argi]);
+        auto filename = inFile.fileName();
+        filename = filename.left(filename.indexOf(QRegExp("[\\/]")));
+        if (!inFile.open(QIODevice::ReadOnly))
+        {
+            qInfo() << "Couldn't open" << filename << ". Exiting.";
+            return 1;
+        }
+
+        QDataStream inStream(&inFile);
+        inStream.setByteOrder(QDataStream::LittleEndian);
+
+        qint64 sizeInInts = inFile.size() / 4;
+        quint32 checkSum = 0x12345678;
+
+        for (qint64 curIntIndex = 0; curIntIndex < sizeInInts - 1; curIntIndex++)
+        {
+            qint32 curInt;
+            inStream >> curInt;
+            checkSum += curInt;
+        }
+
+        quint32 fileCheckSum;
+        inStream >> fileCheckSum;
+
+        inFile.close();
+
+        qInfo() << filename << "reported its checksum as being" << QString::number(fileCheckSum, 16);
+        qInfo() << "checksum_tool calculated its checksum as" << QString::number(checkSum, 16);
+
+        // The \n's here are on purpose so that we get some spacing between files when doing batches
+        if (fileCheckSum == checkSum)
+            qInfo() << "They match!\n";
+        else
+            qInfo() << "They don't match!\n";
     }
-
-    QDataStream inStream(&inFile);
-    inStream.setByteOrder(QDataStream::LittleEndian);
-
-    qint64 sizeInInts = inFile.size() / 4;
-    quint32 checkSum = 0x12345678;
-
-    for (qint64 curIntIndex = 0; curIntIndex < sizeInInts - 1; curIntIndex++)
-    {
-        qint32 curInt;
-        inStream >> curInt;
-        checkSum += curInt;
-    }
-
-    quint32 fileCheckSum;
-    inStream >> fileCheckSum;
-
-    qInfo() << "The file reported its checksum as being" << QString::number(fileCheckSum, 16);
-    qInfo() << "checksum_tool calculated the file checksum as" << QString::number(checkSum, 16);
 
     return 0;
 }
