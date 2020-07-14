6d96f06b8f2a9b422071f44d5e1a4f427fe291d1
IvanDSM <snesivan1@gmail.com>
2020-07-13 21:20:57 -0300

    KFModTool: Add the beginning of a Texture class

---
+++ Tools/KFModTool/texture.h
@@ -0,0 +1,69 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <QObject>
+#include <QImage>
+#include <QPainter>
+
+class Texture : public QObject
+{
+    Q_OBJECT
+public:
+    enum class PMode {
+        CLUT_4BIT = 0,
+        CLUT_8BIT = 1,
+        DIR_15BIT = 2,
+        DIR_24BIT = 3,
+        MIXED = 4
+    };
+
+signals:
+
+protected:
+    struct CLUT
+    {
+        quint32 bnum;
+        quint16 dx;
+        quint16 dy;
+        quint16 w;
+        quint16 h;
+        std::vector<QColor> clut;
+
+        CLUT(quint32 _bnum, quint16 _dx, quint16 _dy, quint16 _w, quint16 _h, PMode pColor) : bnum(_bnum),
+            dx(_dx), dy(_dy), h(_h)
+        {
+            if (pColor == PMode::CLUT_4BIT)
+                w = _w * 4;
+            else if (pColor == PMode::CLUT_8BIT)
+                w = _w * 2;
+            clut.reserve(_bnum);
+        }
+
+        void addColor(quint16 clutEntry)
+        {
+            const quint8 r = clutEntry & 31,
+                         g = (clutEntry >> 5) & 31,
+                         b = (clutEntry >> 10) & 31,
+                         a = ((clutEntry >> 15) & 1 & (r | g | b)) * 255;
+            clut.emplace_back(r, g, b, a);
+        }
+
+        const QColor &getColor(quint8 index)
+        {
+            return clut.at(index);
+        }
+    };
+
+    QImage texture;
+    QPainter texPainter;
+
+    // TIM Header
+    quint32 id;
+    quint32 flags;
+    PMode pMode;
+    bool cf;
+    CLUT clut;
+
+};
+
+#endif // TEXTURE_H
