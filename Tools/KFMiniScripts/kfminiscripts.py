9a9782fce3bf6be0ce5a6d26fd515d805102bae7
IvanDSM <snesivan1@gmail.com>
2020-10-27 12:46:47 -0300

    KFMiniScripts: Fix minor typo

--- Tools/KFMiniScripts/kfminiscripts.py
+++ Tools/KFMiniScripts/kfminiscripts.py
@@ -5,7 +5,7 @@ import numpy
 import random
 
 ## Creates a random number based on a base value.
-# Used for randomizing entity gold drop value based on the class declaration's gold base value.
+# Used by the game for randomizing entity gold drop value based on the entity class declaration's gold base value.
 # @param number Base value for the randomization (e.g. 40)
 def randomize(number : numpy.int32):
     return (random.randint(0,32767) + random.randint(0,32767) - 32768) * number >> 15
