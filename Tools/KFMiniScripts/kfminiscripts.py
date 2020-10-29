6c46df70d19a98a0e65946f79b1280f5a450facf
IvanDSM <snesivan1@gmail.com>
2020-10-28 22:21:16 -0300

    KFMiniScripts: Cleanup and add newly found randomize2

--- Tools/KFMiniScripts/kfminiscripts.py
+++ Tools/KFMiniScripts/kfminiscripts.py
@@ -4,8 +4,19 @@
 import numpy
 import random
 
+## Replica of C-style rand() function
+# Replicates the behavior of the C rand() function using Python's random library
+def c_rand():
+    return random.randint(0, 32767)
+
 ## Creates a random number based on a base value.
 # Used by the game for randomizing entity gold drop value based on the entity class declaration's gold base value.
 # @param number Base value for the randomization (e.g. 40)
 def randomize(number : numpy.int32):
-    return (random.randint(0,32767) + random.randint(0,32767) - 32768) * number >> 15
+    return (c_rand() + c_rand() - 32768) * number >> 15
+
+## Adds random jitter to a given number.
+# Used by the game for randomizing entity direction?
+# @param number Number to apply jitter to
+def randomize2(number : numpy.int32):
+    return (c_rand() + c_rand()) * number >> 15
