5b7f443d72f8b94603a853e1dbe3cc0177308068
IvanDSM <snesivan1@gmail.com>
2020-11-25 15:49:22 -0300

    KFModTool: Implement TMD loading

--- Tools/KFModTool/model.cpp
+++ Tools/KFModTool/model.cpp
@@ -16,17 +16,192 @@ Model::Model(TFile & tFile, unsigned int fileIndex)
     }
 }
 
-void Model::loadMO(const QByteArray & file)
+void Model::loadMO(const QByteArray &file)
 {
     
 }
 
-void Model::loadRTMD(const QByteArray & file)
+void Model::loadRTMD(const QByteArray &file)
 {
     
 }
 
-void Model::loadTMD(const QByteArray & file)
+void Model::loadTMD(const QByteArray &file)
 {
+    QDataStream in(file);
     
+    // Header section
+    uint32_t id;
+    uint32_t flags;
+    uint32_t nobj;
+    
+    in >> id;
+    if (id != 0x41)
+        KFMTError::error("Model: TMD ID is not 0x41. Bailing out.");
+    
+    in >> flags;
+    if (flags != 0)
+        KFMTError::error( "TMD addresses are not relative. Bailing out.");
+    
+    in >> nobj;
+    
+    
+    quint64 objTableOffset = in.device()->pos();
+
+    for (quint32 curObj = 0; curObj < nobj; curObj++)
+    {
+        quint32 verticesOffset;
+        quint32 vertexCount;
+        quint32 normalsOffset;
+        quint32 normalCount;
+        quint32 primitivesOffset;
+        quint32 primitiveCount;
+        qint32 tempScale;
+
+        in >> verticesOffset;
+        in >> vertexCount;
+        in >> normalsOffset;
+        in >> normalCount;
+        in >> primitivesOffset;
+        in >> primitiveCount;
+        in >> tempScale;
+        scale = static_cast<float>(tempScale) / 4096.f;
+
+        verticesOffset += objTableOffset;
+        normalsOffset += objTableOffset;
+        primitivesOffset += objTableOffset;
+
+        baseMesh.vertices.resize(vertexCount);
+        normals.resize(normalCount);
+        primitives.resize(primitiveCount);
+
+        // Read object vertices
+        in.device()->seek(verticesOffset);
+        for (auto &vertex : baseMesh.vertices)
+            vertex.readSVECTOR(in);
+
+        // Read object normals
+        in.device()->seek(normalsOffset);
+        for (auto &normal : normals)
+            normal.readSVECTOR(in);
+
+        // Read object primitives
+        in.device()->seek(primitivesOffset);
+        for (auto &primitive : primitives)
+        {
+            in >> primitive;
+        }
+    }
+}
+
+void Model::Vec3::readSVECTOR(QDataStream & in)
+{
+    short vx;
+    short vy;
+    short vz;
+    
+    in >> vx;
+    in >> vy;
+    in >> vz;
+    in.skipRawData(2);
+    
+    x = static_cast<float>(vx) / 4096.f;
+    y = static_cast<float>(vy) / 4096.f;
+    z = static_cast<float>(vz) / 4096.f;
+}
+
+QDataStream &operator>>(QDataStream & in, Model::Primitive & primitive)
+{
+    uint8_t tempByte;
+    
+    // The elements are read in inverse order because the PS1 is a little endian architecture,
+    // and this entire section is read as one 32-bit integer to save speed.
+    in >> primitive.olen;
+    in >> primitive.ilen;
+    
+    in >> tempByte;
+    primitive.flag = static_cast<Model::Primitive::PrimitiveFlag>(tempByte);
+    
+    in >> tempByte;
+    primitive.mode = static_cast<Model::Primitive::PrimitiveMode>(tempByte);
+    
+    switch(primitive.mode)
+    {
+        case(Model::Primitive::PrimitiveMode::TriFlatNoTexOpaqueLit):
+            in >> primitive.r0;
+            in >> primitive.g0;
+            in >> primitive.b0;
+            in.skipRawData(1);
+            if (Model::Primitive::isGradation(primitive.flag))
+            {
+                in >> primitive.r1;
+                in >> primitive.g1;
+                in >> primitive.b1;
+                in.skipRawData(1);
+                in >> primitive.r2;
+                in >> primitive.g2;
+                in >> primitive.b2;
+                in.skipRawData(1);
+            }
+            in >> primitive.normal0;
+            in >> primitive.vertex0;
+            in >> primitive.vertex1;
+            in >> primitive.vertex2;
+            break;
+        case(Model::Primitive::PrimitiveMode::TriFlatTexOpaqueLit):
+            in >> primitive.u0;
+            in >> primitive.v0;
+            in >> primitive.cba;
+            in >> primitive.u1;
+            in >> primitive.v1;
+            in >> primitive.tsb;
+            in >> primitive.u2;
+            in >> primitive.v2;
+            in.skipRawData(2);
+            in >> primitive.normal0;
+            in >> primitive.vertex0;
+            in >> primitive.vertex1;
+            in >> primitive.vertex2;
+            break;
+        case(Model::Primitive::PrimitiveMode::TriGouraudTexOpaqueLit):
+            in >> primitive.u0;
+            in >> primitive.v0;
+            in >> primitive.cba;
+            in >> primitive.u1;
+            in >> primitive.v1;
+            in >> primitive.tsb;
+            in >> primitive.u2;
+            in >> primitive.v2;
+            in.skipRawData(2);
+            in >> primitive.normal0;
+            in >> primitive.vertex0;
+            in >> primitive.normal1;
+            in >> primitive.vertex1;
+            in >> primitive.normal2;
+            in >> primitive.vertex2;
+            break;
+        case(Model::Primitive::PrimitiveMode::TriGouraudTexTranslucentLit):
+            in >> primitive.u0;
+            in >> primitive.v0;
+            in >> primitive.cba;
+            in >> primitive.u1;
+            in >> primitive.v1;
+            in >> primitive.tsb;
+            in >> primitive.u2;
+            in >> primitive.v2;
+            in.skipRawData(2);
+            in >> primitive.normal0;
+            in >> primitive.vertex0;
+            in >> primitive.normal1;
+            in >> primitive.vertex1;
+            in >> primitive.normal2;
+            in >> primitive.vertex2;
+            break;
+        default:
+            KFMTError::error(QString::asprintf("Model: TMD: Unsupported mode 0x%x. Please implement!\n",
+                                               static_cast<unsigned int>(primitive.mode)));
+            break;
+    }
+    
+    return in;
 }
--- Tools/KFModTool/model.h
+++ Tools/KFModTool/model.h
@@ -16,7 +16,7 @@
 class Model
 {
 public:
-    // Internal structures
+    // Internal enums and structures
     
     /*!
      * \brief Structure for a XYZ vertex.
@@ -26,6 +26,8 @@ public:
         float x;
         float y;
         float z;
+        
+        void readSVECTOR(QDataStream &in);
     };
     
     /*!
@@ -34,6 +36,58 @@ public:
     struct Animation
     {
         std::vector<size_t> frameIndexes;
+    };
+    
+    /*!
+     * \brief Structure for a TMD primitive.
+     */
+    struct Primitive
+    {
+        /*!
+         * \brief Enum with terribly long names for the primitive flags.
+         * Single/double faced only applies to polygon primitives.
+         */
+        enum class PrimitiveFlag;
+        
+        /*!
+         * \brief Enum with terribly long names for the TMD primitive modes.
+         */
+        enum class PrimitiveMode;
+        
+        PrimitiveFlag flag;
+        PrimitiveMode mode;
+        uint8_t ilen;
+        uint8_t olen;
+    
+        // The following variables represent packet elements. Not all of them are present in all types,
+        // and the only reason all of them are declared here is to save time and not have to do some
+        // sketchy inheritance stuff that'd make the file 5x bigger.
+        uint8_t r0, g0, b0;
+        uint8_t r1, g1, b1;
+        uint8_t r2, g2, b2;
+    
+        uint8_t u0, v0;
+        uint8_t u1, v1;
+        uint8_t u2, v2;
+    
+        uint16_t cba;
+        uint16_t tsb;
+    
+        uint16_t vertex0, vertex1, vertex2, vertex3;
+        uint16_t normal0, normal1, normal2, normal3;
+        
+        /*!
+         * \brief Checks whether a primitive's flag indicates it is a gradation primitive.
+         * \param flag Primitive flag
+         * \return Whether the primitive is a gradation primitive.
+         */
+        static bool isGradation(PrimitiveFlag flag) { return static_cast<uint8_t>(flag) > 3; }
+        /*!
+         * \brief Checks whether a primitive's flag indicates it is a single color primitive.
+         * \param flag Primitive flag
+         * \return Whether the primitive is a single color primitive.
+         */
+        static bool isSingleColor(PrimitiveFlag flag) { return static_cast<uint8_t>(flag) < 4; }
     };
     
     /*!
@@ -50,15 +104,86 @@ public:
     
     Mesh baseMesh;
     std::vector<Vec3> normals;
+    std::vector<Primitive> primitives;
+    float scale;
     
     std::vector<Animation> animations;
     std::vector<Mesh> animFrames;
     
 private:
-    
     void loadMO(const QByteArray &file);
     void loadRTMD(const QByteArray &file);
     void loadTMD(const QByteArray &file);
+};
+
+/*!
+ * \brief Operator for reading a primitive from a QDataStream
+ * \param in QDataStream to read from
+ * \param primitive Primitive to read into
+ * \return The QDataStream after reading
+ */
+QDataStream &operator>>(QDataStream &in, Model::Primitive& primitive);
+
+// Enum definitions
+
+enum class Model::Primitive::PrimitiveFlag
+{
+    SingleColorSingleFaceLightSourceNoCalc = 0b00000000, ///< 0x0
+    SingleColorSingleFaceLightSourceCalc = 0b00000001, ///< 0x1
+    SingleColorDoubleFaceLightSourceNoCalc = 0b00000010, ///< 0x2
+    SingleColorDoubleFaceLightSourceCalc = 0b00000011, ///< 0x3
+    GradationSingleFaceLightSourceNoCalc = 0b00000100, ///< 0x4
+    GradationSingleFaceLightSourceCalc = 0b00000101, ///< 0x5
+    GradationDoubleFaceLightSourceNoCalc = 0b00000110, ///< 0x6
+    GradationDoubleFaceLightSourceCalc = 0b00000111 ///< 0x7
+};
+
+enum class Model::Primitive::PrimitiveMode
+{
+    TriFlatNoTexOpaqueLit = 0b00100000, ///< 0x20
+    TriFlatNoTexOpaqueUnlit = 0b00100001, ///< 0x21
+    TriFlatNoTexTranslucentLit = 0b00100010, ///< 0x22
+    TriFlatNoTexTranslucentUnlit = 0b00100011, ///< 0x23
+    TriFlatTexOpaqueLit = 0b00100100, ///< 0x24
+    TriFlatTexOpaqueUnlit = 0b00100101, ///< 0x25
+    TriFlatTexTranslucentLit = 0b00100110, ///< 0x26
+    TriFlatTexTranslucentUnlit = 0b00100111, ///< 0x27
+    TriGouraudNoTexOpaqueLit = 0b00110000, ///< 0x30
+    TriGouraudNoTexOpaqueUnlit = 0b00110001, ///< 0x31
+    TriGouraudNoTexTranslucentLit = 0b00110010, ///< 0x32
+    TriGouraudNoTexTranslucentUnlit = 0b00110011, ///< 0x33
+    TriGouraudTexOpaqueLit = 0b00110100, ///< 0x34
+    TriGouraudTexOpaqueUnlit = 0b00110101, ///< 0x35
+    TriGouraudTexTranslucentLit = 0b00110110, ///< 0x36
+    TriGouraudTexTranslucentUnlit = 0b00110111, ///< 0x37
+    QuadFlatNoTexOpaqueLit = 0b00101000, ///< 0x28
+    QuadFlatNoTexOpaqueUnlit = 0b00101001, ///< 0x29
+    QuadFlatNoTexTranslucentLit = 0b00101010, ///< 0x2a
+    QuadFlatNoTexTranslucentUnlit = 0b00101011, ///< 0x2b
+    QuadFlatTexOpaqueLit = 0b00101100, ///< 0x2c
+    QuadFlatTexOpaqueUnlit = 0b00101101, ///< 0x2d
+    QuadFlatTexTranslucentLit = 0b00101110, ///< 0x2e
+    QuadFlatTexTranslucentUnlit = 0b00101111, ///< 0x2f
+    QuadGouraudNoTexOpaqueLit = 0b00111000, ///< 0x38
+    QuadGouraudNoTexOpaqueUnlit = 0b00111001, ///< 0x39
+    QuadGouraudNoTexTranslucentLit = 0b00111010, ///< 0x3a
+    QuadGouraudNoTexTranslucentUnlit = 0b00111011, ///< 0x3b
+    QuadGouraudTexOpaqueLit = 0b00111100, ///< 0x3c
+    QuadGouraudTexOpaqueUnlit = 0b00111101, ///< 0x3d
+    QuadGouraudTexTranslucentLit = 0b00111110, ///< 0x3e
+    QuadGouraudTexTranslucentUnlit = 0b00111111, ///< 0x3f
+    LineGradationOffOpaque = 0b01000000, ///< 0x40
+    LineGradationOffTranslucent = 0b01000010, ///< 0x42
+    LineGradationOnOpaque = 0b01010000, ///< 0x50
+    LineGradationOnTranslucent = 0b01010010, ///< 0x52
+    SpriteFreeOpaque = 0b01100100, ///< 0x64
+    SpriteFreeTranslucent = 0b01100110, ///< 0x66
+    Sprite1x1Opaque = 0b01101100, ///< 0x6c
+    Sprite1x1Translucent = 0b01101110, ///< 0x6e
+    Sprite8x8Opaque = 0b01110100, ///< 0x74
+    Sprite8x8Translucent = 0b01110110, ///< 0x76
+    Sprite16x16Opaque = 0b01111100, ///< 0x7c
+    Sprite16x16Translucent = 0b01111110 ///< 0x7e
 };
 
 #endif // MODEL_H
